f695f2e4c816f4c003b406036836e926f42288f1
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-12 21:02:15 -0800

    Update __init__.py

--- openbb_tmx/__init__.py
+++ openbb_tmx/__init__.py
@@ -1,1 +1,26 @@
+"""TMX provider module."""
 
+from openbb_provider.abstract.provider import Provider
+from openbb_tmx.models.equity_info import TmxEquityInfoFetcher
+
+tmx_provider = Provider(
+    name="tmx",
+    website="https://www.tmx.com/",
+    description="""
+        TMX Group Companies
+         - Toronto Stock Exchange
+         - TSX Venture Exchange
+         - TSX Trust
+         - Montréal Exchange
+         - TSX Alpha Exchange
+         - Shorcan
+         - CDCC
+         - CDS
+         - TMX Datalinx
+         - Trayport
+    """,
+    required_credentials=None,
+    fetcher_dict={
+        "EquityInfo": TmxEquityInfoFetcher,
+    },
+)
